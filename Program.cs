7ac53ae06c1879ffa88c8d84f02905c29d4e31b8
winiyaz <winiyaz@proton.me>
2025-01-18 20:11:00 -0800

    Structure Project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
 ﻿/*
  Starting the course here 
  */
-Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    // Main function which doesnt have an output but calling the function below it
+    static void Main()
+    {
+        PrintToConsole();
+    }
+
+    // These functions will be called in Main()
+    static void PrintToConsole()
+    {
+        Console.WriteLine("Print-To-Console: Printed");
+    }
+}
+
--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 # Notes section 
 1. MVC - Model View Controller Architecture of the Csharp Applications 
-1. `.sln` - Collection of several projects
+2. `.sln` - Collection of several projects
+3. `.cs` - Csharp File  
+4. `NameSpaces` - Set of classes seperated by name
+	1. Classes similar to python 
+	1. Done to avoid collision - So same naem classes in different namespaces 
