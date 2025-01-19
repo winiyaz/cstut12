a3be70064f04476d3bae13cb5a4c3c156e404faa
winiyaz <winiyaz@proton.me>
2025-01-18 20:36:45 -0800

    Settting up functions

--- Program.cs
+++ Program.cs
@@ -10,12 +10,51 @@ class Program
     static void Main()
     {
         PrintToConsole();
+        MyPrintaz(); // Car Graphic 
+        VariableAndTypes();
     }
 
     // These functions will be called in Main()
     static void PrintToConsole()
     {
         Console.WriteLine("Print-To-Console: Printed");
+    }
+
+    // This is my function
+    static void MyPrintaz()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.WriteLine(@"
+Greeningz -- Zoom...zoom...zoom
+  -           __
+ --          ~( @\   \
+---   _________]_[__/_>________
+     /  ____ \ <>     |  ____  \
+    =\_/ __ \_\_______|_/ __ \__D
+________(__)_____________(__)____
+
+");
+        Console.ResetColor();
+    }
+
+    static void VariableAndTypes()
+    {
+        // Understanding data datypes here 
+        Console.ForegroundColor= ConsoleColor.DarkCyan;
+
+        Console.WriteLine(@"
+-----------------------
+Variables and Types
+-----------------------
+");
+        
+        //Integer variables - Signed or Unsigned 
+        int age = 20;
+        Console.WriteLine(age);
+
+
+        Console.ResetColor();
     }
 }
 
